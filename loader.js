9c5490492d82f795f95e6a4a31391f79428583bd
yakoff90 <yakovenkozhenia90@gmail.com>
2025-11-23 14:27:25 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -56,33 +56,10 @@
             bg: "СЕРИАЛ",      
             he: "סִדְרָה",  
             cs: "SERIÁL" 
-        },
-        maxsm_themes_incardtemplate: {
-            ru: "Макет содержимого карточки",
-            en: "Card content layout",
-            uk: "Макет вмісту картки",
-            be: "Макет змесціва карткі",
-            zh: "卡片内容布局",
-            pt: "Layout do conteúdo do cartão",
-            bg: "Оформление на съдържанието в картата",
-            he: "פריסת תוכן בכרטיס",
-            cs: "Rozvržení obsahu karty"
-        },
-        maxsm_themes_bigbuttons: {
-            ru: "Большие кнопки в карточке",
-            en: "Large buttons in card",
-            uk: "Великі кнопки в картці",
-            be: "Вялікія кнопкі ў картцы",
-            zh: "卡片中的大按钮",
-            pt: "Botões grandes no cartão",
-            bg: "Големи бутони в картата",
-            he: "כפתורים גדולים בכרטיס",
-            cs: "Velká tlačítka v kartě"
         }
     });
 
-    var themes_svg = '<!-- icon666.com - MILLIONS OF FREE VECTOR ICONS --><svg viewBox="0 0 512.00026 512" xmlns="http://www.w3.org/2000/svg"><path d="m491.238281 20.761719c-14.375-14.375-34.265625-21.890625-54.550781-20.625-20.289062 1.269531-39.078125 11.207031-51.550781 27.261719l-98.660157 127.007812-41.109374-41.109375c-12.015626-12.019531-27.996094-18.636719-44.988282-18.636719-16.996094 0-32.972656 6.617188-44.992187 18.636719l-142.363281 142.363281c-17.363282 17.363282-17.363282 45.617188 0 62.980469l180.335937 180.335937c8.679687 8.683594 20.085937 13.023438 31.488281 13.023438 11.40625 0 22.808594-4.339844 31.492188-13.023438l142.363281-142.363281c12.019531-12.019531 18.636719-27.996093 18.636719-44.992187 0-16.992188-6.617188-32.972656-18.636719-44.988282l-41.109375-41.109374 127.007812-98.660157c16.054688-12.472656 25.992188-31.261719 27.261719-51.550781 1.269531-20.292969-6.25-40.175781-20.625-54.550781zm-276.386719 456.722656-15.898437-15.898437 22.957031-22.957032c5.933594-5.9375 5.933594-15.558594 0-21.496094-5.933594-5.933593-15.558594-5.933593-21.492187 0l-22.957031 22.957032-10.152344-10.148438 44.210937-44.210937c5.9375-5.933594 5.9375-15.558594 0-21.492188-5.933593-5.9375-15.558593-5.9375-21.492187 0l-44.210938 44.210938-42.265625-42.265625 22.957031-22.957032c5.9375-5.9375 5.9375-15.558593 0-21.496093-5.933593-5.933594-15.558593-5.933594-21.492187 0l-22.957031 22.957031-10.152344-10.148438 44.210938-44.210937c5.9375-5.933594 5.9375-15.558594 0-21.492187-5.933594-5.9375-15.558594-5.9375-21.492188 0l-44.210938 44.210937-15.898437-15.898437c-5.511719-5.511719-5.511719-14.484376 0-19.996094l77.199219-77.195313 200.328125 200.328125-77.199219 77.199219c-5.511719 5.511719-14.480469 5.511719-19.992188 0zm118.6875-98.695313-200.328124-200.328124 18.175781-18.175782 200.328125 200.328125zm53.40625-67.167968c0 8.875-3.457031 17.222656-9.734374 23.496094l-4 4.003906-191.484376-191.480469-8.847656-8.847656 4.003906-4.003907c6.273438-6.277343 14.621094-9.730468 23.496094-9.730468s17.21875 3.453125 23.492188 9.730468l153.339844 153.335938c6.277343 6.277344 9.734374 14.621094 9.734374 23.496094zm94.578126-238.210938c-.726563 11.589844-6.398438 22.324219-15.570313 29.449219l-130.019531 101-27.796875-27.792969 101.003906-130.019531c7.125-9.171875 17.855469-14.847656 29.449219-15.570313 11.578125-.71875 22.945312 3.566407 31.15625 11.777344 8.210937 8.210938 12.503906 19.570313 11.777344 31.15625zm0 0" fill="#000000" style="fill: rgb(255, 255, 255);"></path><path d="m439.84375 56.953125c-7.949219 0-15.566406 6.992187-15.195312 15.199219.367187 8.234375 6.675781 15.199218 15.195312 15.199218 7.953125 0 15.566406-6.988281 15.199219-15.199218-.367188-8.234375-6.675781-15.199219-15.199219-15.199219zm0 0" fill="#000000" style="fill: rgb(255, 255, 255);"></path></svg>';
-
+    // Основной объект плагина
     var maxsm_themes = {
         name: 'maxsm_themes',
         version: '2.6.1',
@@ -94,7 +71,7 @@
     var prevtheme = '';
     var onetime = false;
 
-    // Ось тут замінюємо блок завантажувача на новий
+    // Цвета loader'а для каждой темы
     var loaderColors = {
         "default": '#fff',
         violet_blue: '#6a11cb',
@@ -105,125 +82,44 @@
         velvet_sakura: '#f6a5b0'
     };
 
-    // Функція для застосування тем
+    // Функция для применения тем
     function applyTheme(theme) {
         $('#maxsm_themes_theme').remove();
 
-        if (
-            prevtheme !== '' &&
-            (
-                (prevtheme === 'default' && theme !== 'default') ||
-                (prevtheme !== 'default' && theme === 'default')
-            )
-        ) {
+        if (prevtheme !== '' && ((prevtheme === 'default' && theme !== 'default') || (prevtheme !== 'default' && theme === 'default'))) {
             window.location.reload();
         }
 
         prevtheme = theme;
 
-        if (theme === 'default') {
-            removeAdditionalSettings();
-            return;
-        }
+        if (theme === 'default') return;
 
         var color = loaderColors[theme] || loaderColors["default"];
+        var svgCode = encodeURIComponent("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"135\" height=\"140\" fill=\"".concat(color, "\"><rect width=\"10\" height=\"40\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"0s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"0s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect><rect width=\"10\" height=\"40\" x=\"20\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"0.2s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"0.2s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect><rect width=\"10\" height=\"40\" x=\"40\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"0.4s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"0.4s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect><rect width=\"10\" height=\"40\" x=\"60\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"0.6s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"0.6s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect><rect width=\"10\" height=\"40\" x=\"80\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"0.8s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"0.8s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect><rect width=\"10\" height=\"40\" x=\"100\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"1s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"1s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect><rect width=\"10\" height=\"40\" x=\"120\" y=\"100\" rx=\"6\"><animate attributeName=\"height\" begin=\"1.2s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"40;100;40\" keyTimes=\"0;0.5;1\"/><animate attributeName=\"y\" begin=\"1.2s\" calcMode=\"linear\" dur=\"1s\" repeatCount=\"indefinite\" values=\"100;40;100\" keyTimes=\"0;0.5;1\"/></rect></svg>"));
 
-        // GIF для завантажувача
-        var gifUrl = "https://raw.githubusercontent.com/ko3ik/LMP/main/wwwroot/logo.gif";
-        
         var style = $('<style id="maxsm_themes_theme"></style>');
-        
-        // Оновлені стилі для завантажувача
-        var loaderStyles = `
-.screensaver__preload {
-    background: url("${gifUrl}") no-repeat 50% 50% !important;
-    background-size: 120px 120px !important;
-    pointer-events: none !important;
-}
-.activity__loader {
-    background: url("${gifUrl}") no-repeat 50% 50% !important;
-    background-size: 80px 80px !important;
-    display: none !important;
-    pointer-events: none !important;
-}
-.activity--load .activity__loader,
-.activity--preload .activity__loader {
-    display: block !important;
-}
-`;
 
-        // Стилі для тем
         var themes = {
-            mint_dark: loaderStyles + ` ... ваші інші стилі для mint_dark ... `,
-            crystal_cyan: loaderStyles + ` ... інші стилі для crystal_cyan ... `,
-            velvet_sakura: loaderStyles + ` ... інші стилі для velvet_sakura ... `
+            mint_dark: "\n.navigation-bar__body\n{background: rgba(18, 32, 36, 0.96);\n}\n.card__quality,\n .card__type::after  {\nbackground: linear-gradient(to right, #1e6262dd, #3da18ddd);\n}\n.screensaver__preload {\nbackground:url(\"data:image/svg+xml,".concat(svgCode, "\") no-repeat 50% 50%\n}\n.activity__loader {\nposition:absolute;\ntop:0;\nleft:0;\nwidth:100%;\nheight:100%;\ndisplay:none;\nbackground:url(\"data:image/svg+xml,").concat(svgCode, "\") no-repeat 50% 50%\n \n}\nhtml, body, .extensions\n {\nbackground: linear-gradient(135deg, #0a1b2a, #1a4036);\ncolor: #ffffff;\n}\n.company-start.icon--broken .company-start__icon,\n.explorer-card__head-img > img,\n.bookmarks-folder__layer,\n.card-more__box,\n.card__img\n,.extensions__block-add\n,.extensions__item\n {\nbackground-color: #1e2c2f;\n}\n.search-source.focus,\n.simple-button.focus,\n.menu__item.focus,\n.menu__item.traverse,\n.menu__item.hover,\n.full-start__button.focus,\n.full-descr__tag.focus,\n.player-panel .button.focus,\n.full-person.selector.focus,\n.tag-count.selector.focus,\n.full-review.focus {\nbackground: linear-gradient(to right, #1e6262, #3da18d);\ncolor: #fff;\nbox-shadow: 0 0.0em 0.4em rgba(61, 161, 141, 0.0);\n}\n.selectbox-item.focus,\n.settings-folder.focus,\n.settings-param.focus {\nbackground: linear-gradient(to right, #1e6262, #3da18d);\ncolor: #fff;\nbox-shadow: 0 0.0em 0.4em rgba(61, 161, 141, 0.0);\nborder-radius: 0.5em 0 0 0.5em;\n}\n.full-episode.focus::after,\n.card-episode.focus .full-episode::after,\n.items-cards .selector.focus::after,  \n.card-more.focus .card-more__box::after,\n.card-episode.focus .full-episode::after,\n.card-episode.hover .full-episode::after,\n.card.focus .card__view::after,\n.card.hover .card__view::after,\n.torrent-item.focus::after,\n.online-prestige.selector.focus::after,\n.online-prestige--full.selector.focus::after,\n.explorer-card__head-img.selector.focus::after,\n.extensions__item.focus::after,\n.extensions__block-add.focus::after,\n.full-review-add.focus::after {\nborder: 0.2em solid #3da18d;\nbox-shadow: 0 0 0.8em rgba(61, 161, 141, 0.0);\n}\n.head__action.focus,\n.head__action.hover {\nbackground: linear-gradient(45deg, #3da18d, #1e6262);\n}\n.modal__content {\nbackground: rgba(18, 32, 36, 0.96);\nborder: 0em solid rgba(18, 32, 36, 0.96);\n}\n.settings__content,\n.settings-input__content,\n.selectbox__content,\n.settings-input {\nbackground: rgba(18, 32, 36, 0.96);\n}\n.torrent-serial {\nbackground: rgba(0, 0, 0, 0.22);\nborder: 0.2em solid rgba(0, 0, 0, 0.22);\n}\n.torrent-serial.focus {\nbackground-color: #1a3b36cc;\nborder: 0.2em solid #3da18d;\n}\n"
         };
 
         style.html(themes[theme] || '');
+
         $('head').append(style);
 
-        if (onetime === false) {
-            onetime = true;
-            forall();
-            removeFromSettingsMenu();
-            fix_lang();
-        }
-    }
+        // Застосовуємо зміщення елементів вниз
+        $('body').css('display', 'flex');
+        $('body').css('flex-direction', 'column');
+        $('body').css('min-height', '100vh');
 
-    function fix_lang() {
-        // Додаємо мовні налаштування
-    }
-
-    function removeAdditionalSettings() {
-        // Прибираємо налаштування, які не потрібні
+        // Додаємо кнопки внизу
+        $('.full-start-new__buttons').css('margin-top', 'auto');
     }
 
     function startPlugin() {
-        var availableThemes = ['mint_dark', 'deep_aurora', 'crystal_cyan', 'amber_noir', 'velvet_sakura', 'default'];
-
-        if (!localStorage.getItem('maxsm_themes_animations')) {
-            localStorage.setItem('maxsm_themes_animations', 'true');
-        }
-
-        Lampa.SettingsApi.addComponent({
-            component: "maxsm_themes",
-            name: Lampa.Lang.translate('maxsm_themes'),
-            icon: themes_svg
-        });
-
-        Lampa.SettingsApi.addParam({
-            component: 'maxsm_themes',
-            param: {
-                name: 'maxsm_themes_selected',
-                type: 'select',
-                values: {
-                    mint_dark: 'Mint Dark',
-                    deep_aurora: 'Deep Aurora',
-                    crystal_cyan: 'Crystal Cyan',
-                    amber_noir: 'Amber Noir',
-                    velvet_sakura: 'Velvet Sakura',
-                    default: 'LAMPA'
-                },
-                "default": 'Mint Dark'
-            },
-            field: {
-                name: Lampa.Lang.translate('maxsm_themes_theme'),
-                description: ''
-            },
-            onChange: function onChange(value) {
-                maxsm_themes.settings.theme = value;
-                Lampa.Settings.update();
-                applyTheme(value);
-            }
-        });
-
         var savedTheme = Lampa.Storage.get('maxsm_themes_selected', 'mint_dark');
-        if (availableThemes.indexOf(savedTheme) === -1) {
-            Lampa.Storage.set('maxsm_themes_selected', 'mint_dark');
-            savedTheme = 'mint_dark';
-        }
         maxsm_themes.settings.theme = savedTheme;
-        applyTheme(maxsm_themes.settings.theme);
+        applyTheme(savedTheme);
     }
 
     if (window.appready) {
