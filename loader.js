c15d0f30acc1cdd67b9795e5eb3c9046ad964daa
yakoff90 <yakovenkozhenia90@gmail.com>
2025-11-23 13:57:36 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,49 @@
+(function() {
+    'use strict';
+    
+    Lampa.Lang.add({
+        maxsm_themes: {
+            ru: "Темы",
+            en: "Themes",
+            uk: "Теми",
+            be: "Тэмы",
+            zh: "主题",
+            pt: "Temas",
+            bg: "Теми",
+            he: "ערכות נושא",
+            cs: "Témata"
+        },
+        maxsm_themes_theme: {
+            ru: "Тема оформления",
+            en: "Interface theme",
+            uk: "Тема оформлення",
+            be: "Тэма афармлення",
+            zh: "界面主题",
+            pt: "Tema de interface",
+            bg: "Тема на интерфейса",
+            he: "ערכת נושא לממשק",
+            cs: "Téma rozhraní"
+        },
+        maxsm_themes_animations: {
+            ru: "Анимации",
+            en: "Animations",
+            uk: "Анімації",
+            be: "Анімацыі",
+            zh: "动画",
+            pt: "Animações",
+            bg: "Анимации",
+            he: "אנימציות",
+            cs: "Animace"
+        },
+        maxsm_themes_translate_tv: {
+            ru: "Переводить TV",
+            en: "Translate TV",
+            uk: "Перекладати TV",
+            be: "Перакладаць TV",
+            zh: "翻译 TV",
+            pt: "Traduzir TV",
+            bg: "Превеждане на TV",
+            he: "לתרגם TV",
+            cs: "Přeložit TV"
+        },
+        maxsm_themes_tvcaption: {
