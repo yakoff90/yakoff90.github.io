212504c7fd8280b41d2afc3c4d9e337c3e7bc40f
yakoff90 <yakovenkozhenia90@gmail.com>
2026-02-17 15:00:20 +0200

    Update all.js

--- all.js
+++ all.js
@@ -11,32 +11,32 @@
           "http://wtch.ch/m", //Онлайн без преміум
           "https://lampame.github.io/main/bo.js", // Бандера Онлайн
           
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
-          "https://вашепосилання",
+          "https://ipavlin98.github.io/lmp-plugins/cardify.js",
+          "https://tvigl.github.io/plugins/quality.js",
+          "https://yakoff90.github.io/QLT.js",
+          "https://mylampa1.github.io/buttons.js",
+          "https://ko31k.github.io/LMP/plugins/rtg.js",
+          "https://yakoff90.github.io/StudiosLogo.js",
+          // "https://вашепосилання",
+          // "https://вашепосилання",
+          // "https://вашепосилання",
+          // "https://вашепосилання",
 
-          "https://вашепосилання",
+          //"https://вашепосилання",
 
           // "https://вашепосилання",
           // "https://вашепосилання",
           // "https://вашепосилання,
 
-          "https://вашепосилання",
+        // "https://вашепосилання",
 
-          "http://вашепосилання",
+         // "http://вашепосилання",
 
-          "https://вашепосилання",
+         // "https://вашепосилання",
 
           "https://icantrytodo.github.io/lampa/torrent_styles_v2.js", //стиль торентів може конфліктувати з іншими стилями
           "https://darkestclouds.github.io/plugins/easytorrent/easytorrent.min.js", //рекомендація торрентів
-          "https://вашепосилання",
+         // "https://вашепосилання",
 
           
         ],
